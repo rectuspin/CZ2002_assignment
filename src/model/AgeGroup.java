cc0b56d4b8b6b2b9e7b197b287cfa297d43f609b
K.S.6 <36289983+E6KS@users.noreply.github.com>
2019-11-08 20:14:33 +0800

    AgeGroup Error Fix

--- src/model/AgeGroup.java
+++ src/model/AgeGroup.java
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
+
 package model;
 
 public enum AgeGroup {
     STANDARD("STANDARD", 0),
-    STUDENT_PRICE("STUDENT PRICE", 2),
+    STUDENT_PRICE("STUDENT PRICE", 0),
     SENIOR_CITIZEN("SENIOR CITIZEN",0),
     CHILDREN("CHILDREN", 0);
 
@@ -24,30 +24,3 @@ public enum AgeGroup {
         this.price = price;
     }
 }
-=======
-package model;
-
-public enum AgeGroup {
-    STANDARD("STANDARD", 0),
-    STUDENT_PRICE("STUDENT PRICE", 2),
-    SENIOR_CITIZEN("SENIOR CITIZEN",0),
-    CHILDREN("CHILDREN", 0);
-
-    private String groupType;
-    private double price;
-    AgeGroup(String groupType, double price){
-        this.groupType = groupType;
-        this.price = price;
-    }
-
-    public String getGroupType(){ return this.groupType; }
-
-    public double getTicketPrice(){
-        return this.price;
-    }
-
-    public void setTicketPrice(double price){
-        this.price = price;
-    }
-}
->>>>>>> 65ec57ebfe207e969a9d6b86e9c3d8b9d543877a
